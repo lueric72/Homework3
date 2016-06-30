43bb4a90c6bf1aff67f36b0af0f645852f636847
ericlu <eric.luyilong@gmail.com>
2016-06-30 19:09:25 +0800

    Initial Commit

---
+++ Homework3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Homework3
+//
+//  Created by Eric on 2016/6/30.
+//  Copyright © 2016年 Eric Lu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
